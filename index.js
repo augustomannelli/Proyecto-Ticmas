6cdc67e8606e1360fd16a6aa7fd72b29b9f1749f
augustomannelli <124205254+augustomannelli@users.noreply.github.com>
2023-03-30 20:33:18 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const boton_email = document.querySelector("#e-mail")
 const boton_direccion = document.querySelector("#direccion")
 const boton_fecha = document.querySelector("#fecha")
 const boton_telefono = document.querySelector("#telefono")
+const boton_habilidades = document.querySelector("#habilidades")
+const boton_experiencia = document.querySelector("#experiencia")
+const boton_estudios = document.querySelector("#estudios")
 console.log(datos)
 boton_nombre.addEventListener("click", function () {
     datos.innerHTML = "Logan Patterson"
@@ -20,4 +23,13 @@ boton_fecha.addEventListener("click", function () {
 })
 boton_telefono.addEventListener("click", function () {
     datos.innerHTML = "(630) 772-2147"
+})
+boton_habilidades.addEventListener("click",function(){
+    datos.innerHTML = "manejo de office<br>Liquidacion de Sueldos e Impuestos"
+})
+boton_experiencia.addEventListener("click",function(){
+    datos.innerHTML = "Encargado General de Hotel<br> Gerente Administracion"
+})
+boton_estudios.addEventListener("click",function(){
+    datos.innerHTML = "primario completo Nuestra Sra.Del Carmen <br> secundario completo Nuestra Sra. Del Carmen<br>Contador En Curso Universidad Nacional MDP "
 })
